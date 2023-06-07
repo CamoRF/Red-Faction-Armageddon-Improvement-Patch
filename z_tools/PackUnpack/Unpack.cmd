0f4714f82c37c0c729079bf1cc6af09bdbe7ee72
Camo <113881920+CamoRF@users.noreply.github.com>
2023-06-07 13:33:32 +1000

    Added unpacked vanilla GOG files

---
+++ z_tools/PackUnpack/Unpack.cmd
@@ -0,0 +1,10 @@
+Gibbed.SaintsRow3.UnpackVPP.exe misc.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe table.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe interface.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe items.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe dlcp04_items.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe dlcp04_misc.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe ruin01.vpp_pc
+Gibbed.SaintsRow3.UnpackVPP.exe mp_inf01d.vpp_pc
+Gibbed.SaintsRow3.UnpackSTR2.exe ns_base.str2_pc
+pause
